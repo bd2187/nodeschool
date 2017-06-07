406c4b471b6c7c4f80d49a3f72b19f9e30ad96c7
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-06-06 21:52:27 -0700

    Complete learnyounode

--- learnyounode/jsonapi.js
+++ learnyounode/jsonapi.js
@@ -4,8 +4,35 @@ var url = require('url');
 var port = process.argv[2] || 3000;
 
 var server = http.createServer(function(req, res){
+  var pathname  = url.parse(req.url).pathname;
+  var parsedURL = url.parse(req.url, true);
+  var time      = new Date(parsedURL.query.iso);
+  var result;
 
-});
+  res.writeHead(200, { 'Content-Type': 'application/json' });
 
-server.listen(port);
-console.log(`Listening to port: ${port}`)
+  if (pathname === '/api/parsetime') {
+    result = parsetime(time);
+    res.end( JSON.stringify(result) );
+  } else if (pathname === '/api/unixtime') {
+    result = unixtime(time);
+    res.end( JSON.stringify(result) );
+  }
+
+}).listen(port);
+console.log(`Listening to port: ${port}`);
+
+function parsetime(time) {
+  return {
+    "hour": time.getHours(),
+    "minute": time.getMinutes(),
+    "second": time.getSeconds()
+  }
+}
+
+function unixtime(time) {
+  var unix = time.getTime();
+  return {
+    "unixtime": unix
+  }
+}
